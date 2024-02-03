f3aa26c3b9e76016dd7bcbec814dac189e8d1ae4
anshmehta <mehta.ansh1@northeastern.edu>
2024-02-03 15:52:08 -0500

    Initial Commit

---
+++ newsswipe/newsswipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsswipeApp.swift
+//  newsswipe
+//
+//  Created by Ansh Mehta on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct newsswipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
